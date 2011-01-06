ced1c1fff22b3f7ccdc3b5790bcb421be8c98f0c
Denis Oliveira <denisbito@gmail.com>
2011-01-06 15:51:54 -0200

    leftover class name changes and small modifications to CertManager and related test

--- src/main/java/br/gov/senado/signa/CertManager.java
+++ src/main/java/br/gov/senado/signa/CertManager.java
@@ -2,7 +2,7 @@ package br.gov.senado.signa;
 
 public class CertManager {
 
-	public static void findCertificates() {
+	public static void findUserCertificates() {
 		// TODO Auto-generated method stub
 
 	}
--- src/test/java/br/gov/senado/signa/CertManagerTest.java
+++ src/test/java/br/gov/senado/signa/CertManagerTest.java
@@ -1,11 +1,34 @@
 package br.gov.senado.signa;
 
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.Provider;
+import java.security.Security;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CertManagerTest {
 
 	@Test
-	public void testGetUserCertificates() {
-		CertManager.findCertificates();
+	public void testCreateUserCertificateAndStoreInJavaKeystore() {
+		Assert.assertEquals(1, 2);
 	}
+
+	@Test
+	public void testGetUserCertificates() throws KeyStoreException {
+		KeyStore keyStore = KeyStore.getInstance("jks");
+		Assert.assertNotNull(keyStore);
+
+		System.out.println(keyStore.aliases());
+	}
+
+	@Test
+	public void lhufas() {
+		Provider[] providers = Security.getProviders();
+		for (Provider provider : providers) {
+			System.out.println(provider);
+		}
+	}
+
 }
