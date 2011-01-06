656dcbbc5f233851af5cbed1eaff1565c080c16f
Denis Oliveira <denisbito@gmail.com>
2011-01-06 19:10:39 -0200

    initial CertManagerTest implementation

--- src/test/java/br/gov/senado/signa/CertManagerTest.java
+++ src/test/java/br/gov/senado/signa/CertManagerTest.java
@@ -1,14 +1,46 @@
 package br.gov.senado.signa;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
-import java.security.Provider;
-import java.security.Security;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import br.gov.senado.signa.util.TestEnvironment;
+
 public class CertManagerTest {
+
+	/**
+	 * Test environment set up.
+	 * 
+	 * Basically we just want to setup a keystore for the tests with
+	 * certificates.
+	 */
+	@BeforeClass
+	public static void setUp() throws KeyStoreException,
+			NoSuchAlgorithmException, CertificateException, IOException {
+		// create a new test Java KeyStore (file in user home directory)
+		KeyStore keyStore = KeyStore.getInstance("jks");
+
+		// before anything else, keystores MUST be loaded. When creating a new
+		// keystore, load it with null arguments!
+		keyStore.load(null, null);
+
+		File userDir = FileUtils.getUserDirectory();
+		FileOutputStream out = new FileOutputStream(new File(userDir,
+				TestEnvironment.KEYSTORE_FILENAME));
+		keyStore.store(out, TestEnvironment.KEYSTORE_PASSWD.toCharArray());
+
+		Assert.assertTrue(new File(userDir, TestEnvironment.KEYSTORE_FILENAME)
+				.exists());
+	}
 
 	@Test
 	public void testCreateUserCertificateAndStoreInJavaKeystore() {
@@ -19,16 +51,6 @@ public class CertManagerTest {
 	public void testGetUserCertificates() throws KeyStoreException {
 		KeyStore keyStore = KeyStore.getInstance("jks");
 		Assert.assertNotNull(keyStore);
-
-		System.out.println(keyStore.aliases());
-	}
-
-	@Test
-	public void lhufas() {
-		Provider[] providers = Security.getProviders();
-		for (Provider provider : providers) {
-			System.out.println(provider);
-		}
 	}
 
 }
---
+++ src/test/java/br/gov/senado/signa/util/TestEnvironment.java
@@ -0,0 +1,13 @@
+package br.gov.senado.signa.util;
+
+/**
+ * Useful test environment constants and methods.
+ * 
+ * @author Denis Oliveira
+ */
+public class TestEnvironment {
+
+	public static String KEYSTORE_FILENAME = ".signa-test-keystore";
+	public static String KEYSTORE_PASSWD = "test-passwd";
+
+}
