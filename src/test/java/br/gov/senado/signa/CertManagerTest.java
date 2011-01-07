9f57da0607511ff264a8177ec59de2d7f733fcfc
Denis Oliveira <denisbito@gmail.com>
2011-01-07 18:59:52 -0200

    added certificates file in PKCS12 format (.p12); certificates obtained from cacert.org

--- src/test/java/br/gov/senado/signa/CertManagerTest.java
+++ src/test/java/br/gov/senado/signa/CertManagerTest.java
@@ -1,55 +1,31 @@
 package br.gov.senado.signa;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import br.gov.senado.signa.util.TestEnvironment;
 
 public class CertManagerTest {
 
-	/**
-	 * Test environment set up.
-	 * 
-	 * Basically we just want to setup a keystore for the tests with
-	 * certificates.
-	 */
-	@BeforeClass
-	public static void setUp() throws KeyStoreException,
-			NoSuchAlgorithmException, CertificateException, IOException {
-		// create a new test Java KeyStore (file in user home directory)
-		KeyStore keyStore = KeyStore.getInstance("jks");
+	@Test
+	public void testKeystoreFileExists() throws URISyntaxException {
+		URL url = this.getClass()
+				.getResource(TestEnvironment.KEYSTORE_FILENAME);
+		Assert.assertNotNull(url);
 
-		// before anything else, keystores MUST be loaded. When creating a new
-		// keystore, load it with null arguments!
-		keyStore.load(null, null);
-
-		File userDir = FileUtils.getUserDirectory();
-		FileOutputStream out = new FileOutputStream(new File(userDir,
-				TestEnvironment.KEYSTORE_FILENAME));
-		keyStore.store(out, TestEnvironment.KEYSTORE_PASSWD.toCharArray());
-
-		Assert.assertTrue(new File(userDir, TestEnvironment.KEYSTORE_FILENAME)
-				.exists());
+		File file = new File(url.toURI());
+		Assert.assertTrue(file.exists());
 	}
 
 	@Test
-	public void testCreateUserCertificateAndStoreInJavaKeystore() {
-		Assert.assertEquals(1, 2);
-	}
-
-	@Test
-	public void testGetUserCertificates() throws KeyStoreException {
-		KeyStore keyStore = KeyStore.getInstance("jks");
+	public void testGetUserCertificates() throws Exception {
+		System.out.println();
+		KeyStore keyStore = TestEnvironment.createTestJavaKeyStore();
 		Assert.assertNotNull(keyStore);
 	}
 
--- src/test/java/br/gov/senado/signa/util/TestEnvironment.java
+++ src/test/java/br/gov/senado/signa/util/TestEnvironment.java
@@ -1,13 +1,46 @@
 package br.gov.senado.signa.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
 /**
- * Useful test environment constants and methods.
+ * Constantes e métodos do ambiente de teste.
  * 
  * @author Denis Oliveira
  */
 public class TestEnvironment {
 
-	public static String KEYSTORE_FILENAME = ".signa-test-keystore";
-	public static String KEYSTORE_PASSWD = "test-passwd";
+	public static String KEYSTORE_FILENAME = "/signa-cacert-certificates.p12";
+	public static String KEYSTORE_PASSWD = "signa-pa55wd";
+
+	/**
+	 * Cria a keystore Java de teste, a partir do arquivo
+	 * "resources/signa-cacert-certificates.p12".
+	 * 
+	 * Uma keystore em Java é um local onde se guarda certificados e chaves
+	 * critpográficas, e normalmente é salvo em um arquivo em disco.
+	 * 
+	 * @return a keyStore carregada com os certificados constantes do arquivo.
+	 */
+	public static KeyStore createTestJavaKeyStore() throws KeyStoreException,
+			IOException, NoSuchAlgorithmException, CertificateException,
+			FileNotFoundException, URISyntaxException {
+		// criar uma nova keystore Java
+		KeyStore keyStore = KeyStore.getInstance("PKCS12");
+
+		// carregar a keystore a partir do arquivo
+		File file = new File(TestEnvironment.class.getResource(
+				KEYSTORE_FILENAME).toURI());
+		keyStore.load(new FileInputStream(file), KEYSTORE_PASSWD.toCharArray());
+
+		return keyStore;
+	}
 
 }
